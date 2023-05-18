11f3dd3e2a7d10e0ef775fd7ae7361dba01e29f4
Luffy <jhonsmith981@hotmail.com>
2023-05-18 04:00:43 -0500

    setup mysql fixed

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 -- Script that prepares a MySQL server
 
 CREATE DATABASE IF NOT EXISTS sabbi_db;
-CREATE USER IF NOT EXIST 'sabbi_dev'@'localhost' IDENTIFIED BY 'sabbi_pwd';
+CREATE USER IF NOT EXISTS 'sabbi_dev'@'localhost' IDENTIFIED BY 'sabbi_pwd';
 GRANT ALL PRIVILEGES ON sabbi_db.* TO 'sabbi_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'sabbi_dev'@'localhost';
 FLUSH PRIVILEGES;
