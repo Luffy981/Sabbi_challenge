c7d64a9d1fd6a8bfdd3c54076937800cc3b4f264
Luffy <jhonsmith981@hotmail.com>
2023-05-18 18:42:02 -0500

    survey logic and template

--- web/survey.py
+++ web/survey.py
@@ -3,7 +3,7 @@
 
 from models import storage, questions, answers
 from os import environ
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 
 
 # Application instance
@@ -24,12 +24,44 @@ def get_survey():
 @app.route('/survey/submit', methods=['POST'], strict_slashes=False)
 def submit_survey():
     """ Survey to user """
-    answers = request.form.to_dict()
-    # print(answers)
+    answers = request.get_json()
+    percent = [0.1, 0.1, 0.15, 0.25, 0.4]
+    i = 0
+    total = 0
+    # print("Soy respuest", answers)
+    for question in questions.values():
+        print(answers[question])
+        total += int(answers[question]) * percent[i]
+        i += 1
+    total = round(total, 2)
+    print("TOTAL: ", total)
+    flag = 0
+    if total < 2:
+        message = "Eres perfil conservador 🤓"
+    elif total >= 2 and total < 3.5:
+        message = "Eres perfil moderado 🙂 "
+    else:
+        message = "Eres perfil arriesgado 😎"
+        flag = 1
+
+    if flag == 1:
+        message2 = "Te recomendamos INVERTIR en el Fondo de Fondos Sabbi Hedge Fund FMIV 🚀"
+    else:
+        message2 = "Te recomendamo NO INVERTIR en el Fondo de Fondos Sabbi Hedge Fund FMIV"
 
     # return jsonify({'message': 'Answers sent successfully'})
-    return jsonify(answers)
+    return redirect(url_for('show_results',
+                            message=message,
+                            recomendation=message2))
 
+@app.route('/survey/results', methods=['GET'], strict_slashes=False)
+def show_results():
+    """ Show results page """
+    message = request.args.get('message')
+    recomendation = request.args.get('recommendation')
+    return render_template('results.html',
+                           message=message,
+                           recomendation=recomendation)
 if __name__ == "__main__":
     """ Main function """
     app.run(host='0.0.0.0', port=5000, debug=True)
