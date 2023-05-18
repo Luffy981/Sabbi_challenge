a2914c71d8c48599e550e85506631ba3a998b310
Luffy <jhonsmith981@hotmail.com>
2023-05-17 21:07:30 -0500

    updating user table

---
+++ models/user.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+""" Hold class User """
+
+import models
+from models.basemodel import BaseModel, Base
+import sqlalchemy
+from sqlalchemy import Column, String, Boolean, Integer
+from sqlalchemy.orm import relationship
+from hashlib import md5
+
+
+class User(BaseModel, Base):
+    """ Table representation of a user """
+    __tablename__ = 'users'
+    id = Column(Integer, autoincrement=True, primary_key=True, unique=True)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    status = Column(Boolean, default=True, nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """ Init user """
+        super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value):
+        """ Sets a password with md5 encryption """
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
+
+
